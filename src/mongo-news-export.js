a83ba0df1405fe0c64ffb12ad5b8393a116e5d07
Alberto León <alberto.leon.crespo@gmail.com>
2023-08-18 12:42:04 +0200

    WIP: first steps to perform a stream and chunk export to load in bigquery

--- src/mongo-news-export.js
+++ src/mongo-news-export.js
@@ -1,41 +1,141 @@
-const {New} = require('./mongoose-module');
 const fs = require('fs');
+const { Transform } = require('stream');
+const { New } = require('./mongoose-module');
+const { BigQuery } = require('@google-cloud/bigquery');
+const { AllHtmlEntities } = require('html-entities');
 
-if (!fs.existsSync('datasets/news')) {
-    fs.mkdirSync('datasets/news');
-}
-
-const writeStream = fs.createWriteStream('datasets/news/noticias.csv');
-
-// Escribe el encabezado en el archivo CSV
-writeStream.write('"_id","_feed","createdAt","creator","pubDate","updatedAt","__v","categories","dc_creator","title","content","enclosure","contentHash","content_encoded","imgs","link"\n');
-
-const CHUNK_SIZE = 8000;
-
-async function writeInChunks() {
-    try {
-        // Conexión a MongoDB
-        const count = await New.countDocuments({});
-        let processed = 0;
-
-        while (processed < count) {
-            const newsList = await New.find({}).skip(processed).limit(CHUNK_SIZE);
-            for (const news of newsList) {
-                const csvLine = `"${news._id}","${news._feed}","${news.createdAt}","${news.creator}","${news.pubDate}","${news.updatedAt}","${news.__v}","${news.categories.join('|')}","${news.dc_creator}","${news.title}","${news.content}","${news.enclosure}","${news.contentHash}","${news.content_encoded}","${news.imgs.join('|')}","${news.link}"\n`;
-                writeStream.write(csvLine);
-            }
-
-            processed += CHUNK_SIZE;
-            console.log(`Procesados ${processed} de ${count} registros`);
-        }
-
-        writeStream.end();
-        console.log('CSV escrito exitosamente');
-        process.exit(0);
-    } catch (err) {
-        console.error('Error:', err);
-        writeStream.end();
+let isHistoryMode = false;
+for (let i = 0; i < process.argv.length; i++) {
+    if (process.argv[i] === '--history=true') {
+        isHistoryMode = true;
+        break;
     }
 }
 
-writeInChunks();
+let dateFilter = {};
+if (!isHistoryMode) {
+    const yesterday = new Date();
+    yesterday.setDate(yesterday.getDate() - 1);
+    dateFilter = { createdAt: { $gte: yesterday } };
+}
+
+const CHUNK_SIZE = 8000;
+const htmlEntities = new AllHtmlEntities();
+
+async function writeInChunks() {
+    try {
+        const count = await New.countDocuments(dateFilter);
+        let processed = 0;
+        let chunkNumber = 1;
+        const currentDate = new Date().toISOString().split('T')[0];
+
+        const jsonTransform = new Transform({
+            writableObjectMode: true,
+            transform(chunk, encoding, callback) {
+                this.push(JSON.stringify({
+                    _id: chunk._id || '',
+                    _feed: chunk._feed || '',
+                    createdAt: chunk.createdAt || '',
+                    // ... otros campos ...
+                    content: htmlEntities.encode(chunk.content || ''),
+                    // ... otros campos ...
+                }) + ',\n');
+                callback();
+            }
+        });
+
+        const writeStream = fs.createWriteStream(`datasets/news/news-${currentDate}-${chunkNumber}.json`);
+        writeStream.write('[\n');
+
+        const readStream = New.find(dateFilter).cursor();
+
+        readStream.on('data', (news) => {
+            jsonTransform.write(news);
+            processed++;
+            if (processed % CHUNK_SIZE === 0) {
+                const progress = `${processed}/${count}`;
+                console.log(`Progreso: ${progress}`);
+            }
+        });
+
+        readStream.on('end', () => {
+            jsonTransform.end();
+        });
+
+        jsonTransform.pipe(writeStream, { end: false });
+
+        jsonTransform.on('finish', () => {
+            jsonTransform.unpipe(writeStream);
+            writeStream.end('\n]'); // Cierra el array
+            console.log(`Archivo JSON-${chunkNumber} escrito exitosamente`);
+            chunkNumber++;
+            if (processed < count) {
+                writeStream = fs.createWriteStream(`datasets/news/news-${currentDate}-${chunkNumber}.json`);
+                writeStream.write('[\n');
+                jsonTransform.pipe(writeStream, { end: false });
+            } else {
+                mergeJsonChunks(currentDate, chunkNumber);
+            }
+        });
+
+        jsonTransform.on('error', (err) => {
+            console.error('Error en la transformación:', err);
+        });
+
+    } catch (err) {
+        console.error('Error:', err);
+        throw err;
+    }
+}
+
+function mergeJsonChunks(date, totalChunks) {
+    const mergedData = [];
+
+    for (let i = 1; i <= totalChunks; i++) {
+        const chunkData = fs.readFileSync(`datasets/news/news-${date}-${i}.json`, 'utf8');
+        const parsedData = JSON.parse(chunkData);
+        mergedData.push(...parsedData);
+    }
+
+    fs.writeFileSync(`datasets/news/news-${date}.json`, JSON.stringify(mergedData));
+    console.log(`Archivos JSON fusionados exitosamente en news-${date}.json`);
+
+    loadJsonToBigQuery(`datasets/news/news-${date}.json`);
+}
+
+async function loadJsonToBigQuery(filename) {
+    const projectId = "newbucket-ia";
+    const datasetId = "newsbucketia";
+    const tableId = "news";
+
+    const bigquery = new BigQuery({ projectId });
+
+    const metadata = {
+        sourceFormat: 'NEWLINE_DELIMITED_JSON',
+        autodetect: true,
+        writeDisposition: isHistoryMode ? 'WRITE_TRUNCATE' : 'WRITE_APPEND'
+    };
+
+    const [job] = await bigquery
+        .dataset(datasetId)
+        .table(tableId)
+        .load(filename, metadata);
+
+    console.log(`Job ${job.id} completado.`);
+}
+
+async function main() {
+    try {
+        console.log('Generando archivos JSON por chunks...');
+        await writeInChunks();
+
+    } catch (err) {
+        console.error('Error:', err);
+    }
+}
+
+if (isHistoryMode) {
+    console.log('Como se ha pasado el parametro history=true se cargarán todas las noticias desde el principio de los tiempos');
+}
+
+main();
